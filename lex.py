0208c997d99b94aa04c9cd7e335d771236ee84e3
Sagar <100994834+sagarEmn@users.noreply.github.com>
2026-06-18 00:49:17 +0545

    feat: add definitions of different tokens

--- lex.py
+++ lex.py
@@ -7,3 +7,26 @@ tokens = [
 ] + list(reserved.values())
 
 t_ignore = ' \t'
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    return t
+
+
+def t_VALUE(t):
+    r'[A-Za-z0-9_./\-]+'
+    t.type = reserved.get(t.value.upper(), 'VALUE')
+    return t
+
+
+def t_error(t):
+    raise SyntaxError(f"Illegal character {t.value[0]!r} at line {t.lexer.lineno}")
+
+
+lexer = lex.lex()
