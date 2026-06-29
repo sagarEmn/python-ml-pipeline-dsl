edd3992130b09bf02ff14a99900e478afd18fb2f
Sagar <100994834+sagarEmn@users.noreply.github.com>
2026-06-29 22:49:57 +0545

    feat: import lex from lex.py

--- parser.py
+++ parser.py
@@ -2,3 +2,7 @@
 
 import ply.yacc as yacc
 
+# tokens: yacc needs the list of token names to know the grammar's "terminals"
+# lexer: we'll handle this to parser.parse() so it knows how to tokenize input. 
+
+from lex import tokens, lexer
