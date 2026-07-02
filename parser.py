0641efca7e4b201268c385edfbd0b85d769b308a
Sagar <100994834+sagarEmn@users.noreply.github.com>
2026-07-02 12:41:34 +0545

    feat: add debug p0 p1 p2 lines

--- parser.py
+++ parser.py
@@ -20,32 +20,76 @@ class Command:
 # PLY passes an object p into p_* function automatically
 def p_program(p):
     "program : lines"
+    print("BEFORE p_program:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+
     p[0] = Program(p[1])
+
+    print("AFTER p_program:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
 
 
 def p_lines_single(p):
     "lines : line"
+    print("BEFORE p_lines_single:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+
     if p[1] is None:
         p[0] = []
     else:
         p[0] = [p[1]]
 
+    print("AFTER p_lines_single:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+
 
 def p_lines_multi(p):
     "lines : lines line"
+    print("BEFORE p_lines_multi:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
+
     if p[2] is not None:
         p[1].append(p[2])
     p[0] = p[1]
 
+    print("AFTER p_lines_multi:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
+
 
 def p_line_command_newline(p):
     "line : command NEWLINE"
+    print("BEFORE p_line_command_newline:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
+
     p[0] = p[1]
+
+    print("AFTER p_line_command_newline:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
 
 
 def p_line_blank(p):
     "line : NEWLINE"
+    print("BEFORE p_line_blank:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+
     p[0] = None
+
+    print("AFTER p_line_blank:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
 
 
 # Grammar Rule for each commands:
@@ -53,7 +97,17 @@ def p_line_blank(p):
 
 def p_command_load(p):
     "command : LOAD VALUE"
+    print("BEFORE p_command_load:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
+
     p[0] = Command("LOAD", p[2])
+
+    print("AFTER p_command_load:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
 
 
 def p_command_show(p):
@@ -73,12 +127,30 @@ def p_command_show(p):
 
 def p_command_target(p):
     "command : TARGET VALUE"
+    print("BEFORE p_command_target:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
+
     p[0] = Command("TARGET", p[2])
+
+    print("AFTER p_command_target:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+    print("p[2]:", p[2])
 
 
 def p_command_describe(p):
     "command : DESCRIBE"
+    print("BEFORE p_command_describe:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
+
     p[0] = Command("DESCRIBE")
+
+    print("AFTER p_command_describe:")
+    print("p[0]:", p[0])
+    print("p[1]:", p[1])
 
 
 # p_error is for missing command values
